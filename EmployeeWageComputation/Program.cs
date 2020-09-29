56cfc2e92642299ce18a35d97d24f0674ecd25d3
Mathura <mathuradas333@gmail.com>
2020-09-29 09:08:36 +0530

    [Mathura] Add. Present or Absent code in UC1

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,9 +4,25 @@ namespace EmployeeWageComputation
 {
     class Program
     {
+        const int IS_FULL_TIME = 1;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation in Master Branch");
+            EmployeeWageComputation.Program.PresentAbsent();
+        }
+        static void PresentAbsent()
+        {
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if(empCheck==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
