f835703242152d755e80f536916e2e8d90ec0972
Mathura <mathuradas333@gmail.com>
2020-09-29 09:33:52 +0530

    [Mathura] Add. Monthly wage Computation in UC5

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,6 +7,7 @@ namespace EmployeeWageComputation
         const int IS_FULL_TIME = 2;
         const int IS_PART_TIME = 1;
         const int EMP_RATE_PER_HOUR = 20;
+        const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation in Master Branch");
@@ -17,23 +18,29 @@ namespace EmployeeWageComputation
             //Variables
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
-            //Computation
-            int empCheck = random.Next(0, 3);
-            switch(empCheck)
+            int totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                //Computation
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : "+ empWage);
+            Console.WriteLine("Total Emp Wage : "+totalEmpWage);
         }
     }
 }
