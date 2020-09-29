3ca037eca01a1ee2863574151fa6421dfef14387
Mathura <mathuradas333@gmail.com>
2020-09-29 09:22:09 +0530

    [Mathura] Add. Part Time Wage Computation in UC3

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,7 +4,8 @@ namespace EmployeeWageComputation
 {
     class Program
     {
-        const int IS_FULL_TIME = 1;
+        const int IS_FULL_TIME = 2;
+        const int IS_PART_TIME = 1;
         const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
@@ -18,8 +19,12 @@ namespace EmployeeWageComputation
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
-            if(empCheck==IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if(empCheck==IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if(empCheck==IS_FULL_TIME)
             {
                 empHrs = 8;
             }
