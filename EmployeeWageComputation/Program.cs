5cb8d15d7b181131ffca1e1fe379703a84062645
Mathura <mathuradas333@gmail.com>
2020-09-30 05:55:08 +0530

    [Mathura]Refractor. ComputeEmpWage inside a class

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,17 +5,20 @@ namespace EmployeeWageComputation
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation in Master Branch");
+            EmployeeWageComputation.EmpWage.ComputeEmpWage();
+        }
+    }
+    public class EmpWage
+    {
         const int IS_FULL_TIME = 2;
         const int IS_PART_TIME = 1;
         const int EMP_RATE_PER_HOUR = 20;
         const int NUM_OF_WORKING_DAYS = 20;
         const int MAX_HRS_IN_MONTH = 100;
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Employee Wage Computation in Master Branch");
-            EmployeeWageComputation.Program.ComputeEmpWage();
-        }
-        static void ComputeEmpWage()
+        public static void ComputeEmpWage()
         {
             //Variables
             int empHrs = 0;
